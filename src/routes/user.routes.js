0ba78f242413c979f864ea5b9fbaae61a0ca5a17
Md. Imran Mir <imranmir6677@gmail.com>
2024-03-31 00:28:15 +0600

    Adding user register controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const upload = require("../middlewares/multer.middleware");
 const { registerUser } = require("../controllers/user.controller");
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 module.exports = router;
